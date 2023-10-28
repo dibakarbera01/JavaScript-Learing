b9daa38ea1a705584f2b278d510441d2fdce310c
Dibakar <dibakarbera01@gmail.com>
2023-10-28 20:26:33 +0530

    28-10-2023

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3) // we are using node.js, not browser
+
+console.log(3+3);
+
+// code readability should be easy
+
+let name="dibakar"
+let age=18
+let isLoggedIn=false
+let state = null;
+
+// number => 2 to power 53
+//biginit
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => variable declared but no value assigned
+// symbol => unique
+
+
+// object
+
+console.log(typeof "dibakar");
+console.log(typeof null); // output :- object
+console.log(typeof undefined); // output:- undefined
