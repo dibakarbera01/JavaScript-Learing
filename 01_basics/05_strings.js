584d9a5bb25cfae127e607f6bc57a7eb73430cb5
Dibakar <dibakarbera01@gmail.com>
2023-10-29 16:35:54 +0530

    29-10-2023

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,35 +1,37 @@
 const name = "hitesh"
 const repoCount = 50
 
-// console.log(name + repoCount + " Value");
+//console.log(name + repoCount + " Value");
 
-console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+//console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
 const gameName = new String('hitesh-hc-com')
 
-// console.log(gameName[0]);
+//console.log(gameName[0]);
 // console.log(gameName.__proto__);
 
 
-// console.log(gameName.length);
-// console.log(gameName.toUpperCase());
-console.log(gameName.charAt(2));
-console.log(gameName.indexOf('t'));
+//console.log(gameName.length);
+//console.log(gameName.toUpperCase());
+//console.log(gameName.charAt(2));
+//console.log(gameName.indexOf('t'));
 
-const newString = gameName.substring(0, 4)
-console.log(newString);
+// const newString = gameName.substring(0, 4)
+// console.log(newString);
 
-const anotherString = gameName.slice(-8, 4)
-console.log(anotherString);
+// const Dibakar="Dibakar";
+// const anotherString = Dibakar.slice(-8, 4)
+// console.log(anotherString);
 
-const newStringOne = "   hitesh    "
-console.log(newStringOne);
-console.log(newStringOne.trim());
+// const newStringOne = "   dibakar   "
+// console.log(newStringOne);
+// console.log(newStringOne.trim()); //remove the space
 
 const url = "https://hitesh.com/hitesh%20choudhary"
 
-console.log(url.replace('%20', '-'))
+// console.log(url.replace('%20', '-')) //replace the value
 
-console.log(url.includes('sundar'))
+// console.log(url.includes('sundar')) //check the string exsits or not
 
-console.log(gameName.split('-'));
+const Name="dibakar-bera"
+console.log(Name.split('-'));
